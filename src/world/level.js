14f9e68334c777e27d5a7003805355aa03a9d539
KinectTheUnknown <KinectTheUnknownUnicorn@gmail.com>
2020-06-17 15:01:19 -1000

    Use hex for level.dat prefix

--- src/world/level.js
+++ src/world/level.js
@@ -1,7 +1,7 @@
 const NBTFile = require("../utils/nbt/file")
 //The characters removed to avoid having to parse nbt twice
 //(the useful nbt data is nested in the nbt stored in level.dat)
-const prefix = Buffer.from("\b\u0000\u0000\u0000\b\b\u0000\u0000")
+const prefix = Buffer.from("0800000014080000", "hex")
 module.exports = class Level extends NBTFile {
   constructor(dir, fName = "level.dat", le = true) {
     super(dir, fName, le)
