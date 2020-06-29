1db544bf149b800be0d49af08360aad6a03ec205
KinectTheUnknown <34723558+KinectTheUnknown@users.noreply.github.com>
2020-06-29 01:17:37 -1000

    Created tests for NBT

---
+++ tests/utils/nbt.js
@@ -0,0 +1,60 @@
+const test = require("ava")
+const fs = require("fs").promises
+const path = require("path")
+const helloWorld = {
+  raw: {
+    type: "compound",
+    name: "hello world",
+    value: {
+      name: {
+        type: "string",
+        value: "Bananrama"
+      }
+    }
+  },
+  simplified: {
+    name: "Bananrama"
+  }
+}
+/* eslint-disable global-require */
+
+{
+  const NBT = require("../../src/utils/nbt/")
+
+  test("NBT reading, setting, and creating keys", async t => {
+    const buf = await fs.readFile(path.join(__dirname, "./hello_world.nbt"))
+    const nbt = new NBT(buf, false)
+
+    t.deepEqual(
+      nbt._data, helloWorld.raw,
+      "Should be exactly the same structure"
+    )
+    t.deepEqual(
+      nbt.data,
+      helloWorld.simplified,
+      "Should simplify to the same structure"
+    )
+    t.deepEqual(
+      nbt._get("name"),
+      helloWorld.raw.value.name,
+      "Should return the raw object"
+    )
+    t.true(
+      nbt.get("name") === helloWorld.simplified.name,
+      "Should return the value"
+    )
+    t.false(
+      nbt.set("name", "KinectTheUnknown"),
+      "Should set value, but not create a new key"
+    )
+    t.throws(
+      () => nbt.set("food", "Pizza"),
+      {message: "Tried to create new key, but type was not specified"},
+      "Should not create a new key without a type"
+    )
+    t.true(
+      nbt.set("food", "Pizza", "string"),
+      "Should create a new key with value"
+    )
+  })
+}
