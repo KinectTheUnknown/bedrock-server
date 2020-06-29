137051604ff3871d1a742ec256ab22d6981877d6
KinectTheUnknown <34723558+KinectTheUnknown@users.noreply.github.com>
2020-06-29 01:18:37 -1000

    Fixed NBT.set and NBT.get

--- src/utils/nbt/base.js
+++ src/utils/nbt/base.js
@@ -9,20 +9,33 @@ module.exports = class NBTBase {
   }
   _get(key) {
     const keys = key.split(".")
-    let item = this._data.value
+    let item = this._data
     for (let [i, k] of keys.entries()) {
       if (!item) {
         throw new TypeError(
           `Can't read property '${keys[i - 1]}' (${i - 1}) of undefined`
         )
       }
-      item = k in item ? item[k].value : undefined
+      switch (item.type) {
+        case "list":
+          item = item.value
+          //falls through
+        case "compound":
+          item = item.value[k]
+          break
+        default:
+          throw new Error(
+            "Key is not a list nor compound " + keys.slice(0, i + 1)
+          )
+      }
     }
 
     return item
   }
   get(key) {
-    return nbt.simplify(this._get(key))
+    const res = this._get(key)
+    
+    return res instanceof Array ? res : nbt.simplify(res)
   }
   has(key) {
     let curr = this._data.value
@@ -35,16 +48,39 @@ module.exports = class NBTBase {
 
     return true
   }
-  set(key, val, {create = false} = {}) {
-    const curr = this._get(key)
-    let created = false
-    if (!curr) {
-      if (!create)
-        throw new Error("Key does not exist: " + key)
+  set(key, value, type = null) {
+    let pKey = key.split(".")
+    const name = pKey.pop()
 
-      created = true
+    pKey = pKey.join(".")
+    const parent = pKey ? this._get(pKey) : this._data
+    let created = false
+    if (!parent)
+      throw new Error("Key does not exist " + pKey)
+
+    switch (parent.type) {
+      case "compound":
+        if (name in parent.value)
+          parent.value[name].value = value
+        else {
+          if (!type) {
+            throw new Error(
+              "Tried to create new key, but type was not specified"
+            )
+          }
+          created = true
+          parent.value[name] = {
+            type,
+            value
+          }
+        }
+        break
+      case "list":
+        if (!Number.isInteger(name) || parseInt(name) < 0)
+          throw new RangeError("Index is not a positive integer")
+
+        parent.value.value[name] = value
     }
-    curr.value = val
 
     return created
   }
