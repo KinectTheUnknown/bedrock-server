14033576413f278e827888a1609f8df764323d37
KinectTheUnknown <34723558+KinectTheUnknown@users.noreply.github.com>
2020-07-02 00:42:40 -1000

    Level properly handles header

--- src/world/level.js
+++ src/world/level.js
@@ -1,16 +1,33 @@
 const NBTFile = require("../utils/nbt/file")
 //The characters removed to avoid having to parse nbt twice
 //(the useful nbt data is nested in the nbt stored in level.dat)
-const prefix = Buffer.from("0800000014080000", "hex")
 module.exports = class Level extends NBTFile {
   constructor(dir, fName = "level.dat", le = true) {
     super(dir, fName, le)
   }
+  get prefix() {
+    const buf = new Buffer(8)
+
+    buf.writeInt32LE(this.version, 0)
+    buf.writeInt32LE(this.size, 4)
+
+    return buf
+  }
+  async init() {
+    const res = await super.init()
+
+    this.version = res.version
+    this.size = res.size
+
+    return res
+  }
   toBuffer() {
-    return Buffer.concat([prefix, super.toBuffer()])
+    return Buffer.concat([this.prefix, super.toBuffer()])
   }
   static async parseData(data, le) {
     return {
+      version: data.readUInt32LE(0),
+      size: data.readUInt32LE(4),
       data: await super.parseData(data.slice(8), le)
     }
   }
